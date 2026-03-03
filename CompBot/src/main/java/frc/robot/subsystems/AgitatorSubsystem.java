62c08ffc6536497fdbd2c43446bd6a42303971ad
C-Ham <OrangeEddie10@gmail.com>
2026-03-03 18:55:32 -0500

    Update AgitatorSubsystem.java

--- CompBot/src/main/java/frc/robot/subsystems/AgitatorSubsystem.java
+++ CompBot/src/main/java/frc/robot/subsystems/AgitatorSubsystem.java
@@ -5,13 +5,33 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class AgitatorSubsystem extends SubsystemBase {
   /** Creates a new AgitatorSubsystem. */
-  public AgitatorSubsystem() {}
+  SparkMax agitatorMotor = new SparkMax(956712, MotorType.kBrushless);
+  public AgitatorSubsystem() {
+    //Setting up configurations for the agitatorMotor
+    SparkMaxConfig agitatorConfig = new SparkMaxConfig();
+    agitatorConfig.idleMode(IdleMode.kCoast);
+    agitatorConfig.inverted(false);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public void setAgitatorPower(double agitatorPower) {
+    //This method allows for setting the agitatorMotor's speed
+    agitatorMotor.set(agitatorPower);
+  }
+
+  public void stopAgitator() {
+    //This method allows for stopping the agitatorMotor
+    agitatorMotor.stopMotor();
   }
 }
